ef53696bc0f4fbe71e7e9c64738e99d44b321a40
@sebtheo <Sebastian.Evison-Theodossiades@ons.gov.uk>
2026-02-26 11:03:13 +0000

    fix: demo emoji-slider in chatSurvey

--- packages/template/src/App.tsx
+++ packages/template/src/App.tsx
@@ -45,7 +45,7 @@ import {
 } from '@survey-kit/core'
 import surveyConfig1 from './surveys/survey-1.json'
 import surveyConfig2 from './surveys/survey-2.json'
-import chatSurveyConfig from './surveys/survey-1.json'
+import chatSurveyConfig from './surveys/survey-2.json'
 import layoutConfig from './layouts/layout.config.json'
 import sectionsConfig from './sections/sections.config.json'
 import cookieConfig from './cookies/cookies.config.json'
@@ -78,6 +78,7 @@ const chatComponents = {
   TypingIndicator,
   ChatContainer,
   ChatReviewScreen,
+  EmojiSlider,
 }
 
 /**
