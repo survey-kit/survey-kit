4ec7bfe0990d536cd0cff4fe0a10d8f8cb9c29ac
Sebastian Theo <seb@theo4.uk>
2026-04-03 13:21:56 +0100

    chore: adjust headings css

--- packages/registry/registry/primitives/heading/heading.tsx
+++ packages/registry/registry/primitives/heading/heading.tsx
@@ -6,11 +6,11 @@ import { cn } from '../../../lib/utils'
 const headingVariants = cva('font-semibold tracking-tight', {
   variants: {
     level: {
-      h1: 'text-4xl lg:text-5xl',
-      h2: 'text-3xl lg:text-4xl',
-      h3: 'text-2xl lg:text-3xl',
-      h4: 'text-xl lg:text-2xl',
-      h5: 'text-lg lg:text-xl',
+      h1: 'text-3xl lg:text-4xl',
+      h2: 'text-2xl lg:text-3xl',
+      h3: 'text-xl lg:text-2xl',
+      h4: 'text-lg lg:text-xl',
+      h5: 'text-md lg:text-lg',
       h6: 'text-base lg:text-lg',
     },
     variant: {
